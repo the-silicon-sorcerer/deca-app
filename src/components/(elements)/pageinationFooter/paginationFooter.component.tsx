8908bd6c2cd95d78d1e3cae6c117cfb96e36afcb
Michael Kanahele (MAIN) <Kanahelebusiness@gmail.com>
2023-03-14 14:45:42 -0700

    Fixed Search

--- src/components/(elements)/pageinationFooter/paginationFooter.component.tsx
+++ src/components/(elements)/pageinationFooter/paginationFooter.component.tsx
@@ -1,5 +1,4 @@
 import style from "./paginationFooter.module.css";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 interface PaginationFooterProps {
@@ -19,38 +18,70 @@ const PaginationFooter = ({
 }: PaginationFooterProps) => {
   const generatePagination = (page: number) => {
     const gen = [];
-    for (
-      let i =
-        page - 3 > 0 ? (page + 3 > totPages ? totPages - 6 : page - 3) : 1;
-      i <= (page - 3 > 0 ? (page + 3 > totPages ? totPages : page + 3) : 7);
-      i++
-    ) {
-      gen.push(
-        <Link href={`/${path}/${i}`} key={Math.random()}>
-          <div
-            className={style.paginationBox}
-            style={{
-              backgroundColor: i === page ? color : undefined,
-              color: i === page ? "white" : undefined,
-              border: i === page ? "1px solid black" : undefined,
-            }}
-          >
-            <p className="body-Small">{i}</p>
-          </div>
-        </Link>
-      );
+    if (totPages >= 7) {
+      for (
+        let i =
+          page - 3 > 0 ? (page + 3 > totPages ? totPages - 6 : page - 3) : 1;
+        i <= (page - 3 > 0 ? (page + 3 > totPages ? totPages : page + 3) : 7);
+        i++
+      ) {
+        gen.push(
+          <Link href={`/${path}/${i}`} key={Math.random()}>
+            <div
+              className={style.paginationBox}
+              style={{
+                backgroundColor: i === page ? color : undefined,
+                color: i === page ? "white" : undefined,
+                border: i === page ? "1px solid black" : undefined,
+              }}
+            >
+              <p className="body-Small">{i}</p>
+            </div>
+          </Link>
+        );
+      }
+    } else {
+      for (let i = 1; i <= totPages; i++) {
+        gen.push(
+          <Link href={`/${path}/${i}`} key={Math.random()}>
+            <div
+              className={style.paginationBox}
+              style={{
+                backgroundColor: i === page ? color : undefined,
+                color: i === page ? "white" : undefined,
+                border: i === page ? "1px solid black" : undefined,
+              }}
+            >
+              <p className="body-Small">{i}</p>
+            </div>
+          </Link>
+        );
+      }
     }
 
     return gen;
   };
 
+  if (totPages >= 7) {
+    return (
+      <>
+        <div className={style.paginationContainer}>
+          {generatePagination(page)}
+        </div>
+        {children}
+        <div className={style.paginationContainer}>
+          {generatePagination(page)}
+        </div>
+      </>
+    );
+  }
   return (
     <>
-      <div className={style.paginationContainer}>
+      <div className={style.paginationContainerSmall}>
         {generatePagination(page)}
       </div>
       {children}
-      <div className={style.paginationContainer}>
+      <div className={style.paginationContainerSmall}>
         {generatePagination(page)}
       </div>
     </>
