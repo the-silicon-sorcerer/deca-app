01c766c9c1c024221174ac681d348566c0ef419d
Michael Kanahele (MAIN) <Kanahelebusiness@gmail.com>
2023-03-14 14:50:51 -0700

    Search zoom

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -6,8 +6,9 @@ export default function Head() {
       <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0"
+        content="width=device-width, initial-scale=1., maximum-scale=1"
       ></meta>
+      <meta name="theme-color" content="#000000"></meta>
       <link rel="icon" href="/favicon.ico" />
     </>
   );
